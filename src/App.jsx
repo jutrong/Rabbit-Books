52d0b910a5d875faae3cb5d3dc00db5486fa573b
[SW5] 이주현 <leek71358@gmail.com>
2023-07-06 00:55:04 +0900

    add:mypage

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Style from "./components/pages/StylePage/StylePage";
 import Home from "./components/pages/HomePage/HomePage";
-import Layout from './components/atoms/Layout/Layout.jsx';
+import Mypage from "./components/pages/Mypage/Mypage";
+import Layout from "./components/atoms/Layout/Layout.jsx";
 
 const App = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<Layout/>}>
-                <Route path="/" element={<Home/>}/>
-                <Route path="style" element={<Style/>}/>
-            </Route>
-        </Routes>
-    );
-}
-export default App
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="style" element={<Style />} />
+        <Route path="mypage" element={<Mypage />} />
+      </Route>
+    </Routes>
+  );
+};
+export default App;
--- src/components/pages/Mypage/Mypage.jsx
+++ src/components/pages/Mypage/Mypage.jsx
@@ -1,4 +1,6 @@
 import "./Mypage.css";
+import Profile from "../../../assets/images/rabbit.png";
+import member from "../../../assets/images/icons/icon_member_active.png";
 
 export default function Mypage() {
   return (
@@ -47,8 +49,66 @@ export default function Mypage() {
       </div>
       <div className="main_container">
         <h3>계정 관리</h3>
-        <p>기본정보</p>
-        <div className="main_container_box"></div>
+        <p>회원정보</p>
+        <div className="main_container_box">
+          <div className="profile">
+            <img src={Profile} alt="profile" className="profile_img" />
+          </div>
+          <div className="input_box">
+            <form action="">
+              <div className="name_wrap">
+                <label className="input_box_name" for="mypage_name">
+                  이름
+                </label>
+                <input
+                  type="text"
+                  className="w_276"
+                  id="mypage_name"
+                  placeholder="토깽이"
+                />
+              </div>
+              <div className="mypage_email_box">
+                <div className="email_wrap">
+                  <label className="input_box_email" for="mypage_email">
+                    이메일
+                  </label>
+                </div>
+                <div>
+                  <input
+                    type="text"
+                    className="w_276"
+                    id="mypage_email"
+                    placeholder="elice@tokki.io"
+                  />
+                  <button className="blue_btn w_100 modal_btn">
+                    이메일 변경
+                  </button>
+                </div>
+              </div>
+
+              <div className="mypage_phone_box">
+                <label className="input_box_phone" for="mypage_phone">
+                  휴대폰 번호
+                </label>
+                <div>
+                  <input
+                    type="number"
+                    className="w_276"
+                    id="mypage_phone"
+                    placeholder="010-1234-1234"
+                  />
+                  <button className="blue_btn w_100 modal_btn">
+                    휴대폰 인증
+                  </button>
+                </div>
+              </div>
+              <div className="input_btn_box">
+                <button className="cancle_btn black_btn big w_100">취소</button>
+                <button className="save_btn blue_btn big w_100">저장</button>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   );
